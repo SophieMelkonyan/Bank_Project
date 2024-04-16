1450a920c2e57249f78361d7e760dc4a7662a9bb
Sophie <sophiemelkonyan77@gmail.com>
2024-04-16 13:28:24 +0400

    Messages

--- User/views.py
+++ User/views.py
@@ -6,21 +6,32 @@ from django.contrib.auth.views import LoginView as Login, LogoutView as Logout
 
 User = get_user_model()
 
+from django.contrib import messages
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
 class UserRegistrationView(FormView):
     template_name = 'user/Login.html'
     form_class = CustomUserCreationForm
     model = User
     success_url = '/'
 
-
     def form_valid(self, form):
-
         form.save()
-        messages.success(self.request, 'Account was created successfully!')
-
+        messages.success(self.request, 'Account was created successfully!    ')
         return super().form_valid(form)
 
-class LoginView(Login):
+class LoginView(BaseLoginView):
+    template_name = 'user/login.html'
+    form_class = AuthenticationForm
+
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user)
+        messages.success(self.request, 'You have been logged in successfully!')
+        return super().form_valid(form)
+
     template_name = "user/Login.html"
 
 
