3ba5ebb0a5dcc5d93ccbceb9cfad61b2ce3770a7
Jasmin-Khachatryan <jaskhachatryan68@gmail.com>
2024-04-24 13:29:32 +0400

    user verification

--- User/form.py
+++ User/form.py
@@ -1,12 +1,29 @@
+
+from .models import Profile
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User, Profile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class EmailForm(forms.Form):
+    email = forms.EmailField()
+    subject = forms.CharField()
+    body = forms.CharField(widget=forms.Textarea())
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['email','password1', 'password2', 'first_name', 'last_name',]
+        fields = ['email', 'password1', 'password2', 'first_name', 'last_name',]
+
+        def save(self, commit=True):
+            user = super().save(commit=False)
+
+            if commit:
+                user.save()
+            return user
 
 
 class ProfileForm(forms.ModelForm):
